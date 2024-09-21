4eb1e388b4ee1c308c7dad10794258999539a506
a8dogdark <a8dogdark@gmail.com>
2024-09-21 17:41:59 -0300

    21-09-2024

--- 3.0/ultra30.asm
+++ 3.0/ultra30.asm
@@ -1,21 +1,22 @@
-PCRSR = $CB    
+BAUD600     = $05CC
+BAUD700     = $04F7
+BAUD800     = $0458 
+BAUD900     = $03DB
+BAUD990     = $0381
+BAUD1150    = $0303
+BAUD1400    = $0278
+BAUD2000    = $01B8
     ORG $2100
-    ICL 'BASE/SYS_EQUATES.M65'
+    ICL  'BASE/SYS_EQUATES.M65'
     ICL 'KEM2.ASM'
     ICL 'MEM.ASM'
     ICL 'HEXASCII.ASM'
-SISTEMA
-    .BY 0
-BYTESLEIDOS
-    .BY 0,0,0
-BANCOSTOTALES
-    .BY 0,0
 DLS
 :3  .BY $70
     .BY $46
     .WO SHOW
     .BY $70
-:22 .BY $02
+:22  .BY $02
     .BY $41
     .WO DLS
 SHOW
@@ -23,53 +24,59 @@ SHOW
     .SB +128,"dogdark"
     .SB "  softwares "
     .SB +32,"QRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRE"
-    .SB "| DOGCOPY ULTRA V 3.0  BY DOGDARK 2024 |"
-    .SB +32,"ARRRRRRRRRRRRRRRRRRRRWRRRRRRRRRRRRRRRRRD"
-    .SB "| MEMORIA DISPONIBLE |        "
-MUESTROMEMORIADISPONIBLE
-    .SB "******** |"
-    .SB "| BANCOS DISPONIBLES |             "
-MUESTROBANCOSDISPONIBLES
-    .SB "*** |"
-    .SB "| PORTB EN USO       |             "
-MUESTROPORTB
-    .SB "*** |"
-    .SB "| SISTEMA EN USO     |          "
+    .SB "| DOGCOPY ULTRA V 3.0 BY  DOGDARK 2024 |"
+    .SB +32,"ARRRRRRRRRRRRRRRRRWRRRRRRRRRRRRRRRRRRRRD"
+    .SB "|MEMORIA          |             "
+MUESTROMEMORIA
+    .SB "*******|"
+    .SB "|BANCOS           |                  "
+MUESTROBANCOS
+    .SB "**|"
+    .SB "|SISTEMA A GRABAR |              "
 MUESTROSISTEMA
-    .SB "****** |"
-    .SB "| BAUD A GRABAR      |            "
-MUESTROBAUD
-    .SB "**** |"
-    .SB "| BYTES LEIDOS       |         "
+    .SB "NHP600|"
+    .SB "|VELOCIDAD BAUDIO |                "
+MUESTROVELOCIDAD
+    .SB "0600|"
+    .SB "|PORTB EN USO     |                 "
+MUESTROPORTB
+    .SB "***|"
+    .SB "|BYTES POR BLOQUE |                 "
+MUESTROBYTESPORBLOQUE
+    .SB "128|"
+    .SB "|BYTES LEIDOS     |             "
 MUESTROBYTESLEIDOS
-    .SB "******* |"
-    .SB "| BLOQUES A GRABAR   |            "
-MUESTROBLOQUESLEIDOS
-    .SB "**** |"
-    .SB +32,"ARRRRRRRRRRRWRRRRRRRRXRRRRRRRRRRRRRRRRRD"
-    .SB "| TITULO 01 | "
+    .SB "*******|"
+    .SB "|BLOQUES A GRABAR |                "
+MUESTROBLOQUESAGRABAR
+    .SB "****|"
+    .SB "|TITULO 01        |"
 TITULO01
-    .SB "********************     |"
-    .SB "| TITULO 02 | "
+    .SB "********************|"
+    .SB "|TITULO 02        |"
 TITULO02
-    .SB "********************     |"
-    .SB     "| FUENTE    | "
-FUENTE
-    .SB "********************     |"
-    .SB +32,"ZRRRRRRRRRRRXRRRRRRRRRRRRRRRRRRRRRRRRRRC"
+    .SB "********************|"
+    .SB     "|FUENTE           |"
+FUENTE  
+    .SB "********************|"
+    .SB +32,"ZRRRRRRRRRRRRRRRRRXRRRRRRRRRRRRRRRRRRRRC"
 MUESTRODATA
-    .SB "DATA************************************" ;40
-    .SB "****************************************" ;80
-    .SB "****************************************" ;120
-    .SB "****************************************" ;160
-    .SB "****************************************" ;200
-    .SB "****************************************" ;240
-    .SB "*************************************FIN" ;250
-NHP
-    .SB "   NHP"
-NHP8
-    .SB "  NHP8"
-NHP9
+    .SB "DATA************************************"  ;40
+    .SB "****************************************"  ;80
+    .SB "****************************************"  ;120
+    .SB "****************************************"  ;160
+    .SB "****************************************"  ;200
+    .SB "****************************************"  ;240
+    .SB "*************************************FIN"  ;280
+SISTEMA
+    .BY 0
+NHP600
+    .SB "NHP600"
+NHP700
+    .SB "NHP700"
+NHP800
+    .SB "NHP800"
+NHP900
     .SB "NHP900"
 NHPSTAC
     .SB "  STAC"
@@ -77,229 +84,90 @@ NHPULTRA
     .SB " ULTRA"
 NHPSUPER
     .SB " SUPER"
-
-BAUDIOS600
+NHPEXTRA
+    .SB " EXTRA"
+VEL600
     .SB "0600"
-BAUDIOS800
+VEL700
+    .SB "0700"
+VEL800
     .SB "0800"
-BAUDIOS900
+VEL900
     .SB "0900"
-BAUDIOSSTAC
+VELSTAC
     .SB "0990"
-BAUDIOSULTRA
+VELULTRA
     .SB "1150"
-BAUDIOSSUPER
+VELSUPER
     .SB "1400"
-RY
-	.BYTE 0,0,0
+VELEXTRA
+    .SB "2000"
+BYTESPORBLOQUE
+    .BY 0
+BYTE128
+    .SB "128"
+BYTE255
+    .SB "255"
 ;************************************************
-;FUNCION QUE NOS PERMITE PODER CONVERTIR UN BYTE
-;EN ATASCII, USADO PARA INGRESO DE TITULOS Y
-;FUENTE, NO TIENE LIMITACIONES MAYORES EN LAS
-;PULSACIONES DEL TECLADO
+;FUNCIONES DEL SISTEMA
 ;************************************************
-ASCINT
-	CMP #32
-	BCC ADD64
-	CMP #96
-	BCC SUB32
-	CMP #128
-	BCC REMAIN
-	CMP #160
-	BCC ADD64
-	CMP #224
-	BCC SUB32
-	BCS REMAIN
-ADD64
-	CLC
-	ADC #64
-	BCC REMAIN
-SUB32
-	SEC
-	SBC #32
-REMAIN
-	RTS
-;************************************************
-;FUNCION QUE CIERRA PERIFERICOS
-;************************************************
-CLOSE
-	LDX #$10
-	LDA #$0C
-	STA $0342,X
-	JMP $E456
-;************************************************
-;CURSOR PARPADEANTE
-;************************************************
-FLSH
-	LDY RY
-	LDA (PCRSR),Y
-	EOR #63
-	STA (PCRSR),Y
-	LDA #$10
-	STA $021A
-	RTS
-;************************************************
-;ABRE PERIFERICO TECLADO
-;************************************************
-OPENK
-	LDA #255
-	STA 764
-	LDX #$10
-	LDA #$03
-	STA $0342,X
-	STA $0345,X
-	LDA #$26
-	STA $0344,X
-	LDA #$04
-	STA $034A,X
-	JSR $E456
-	LDA #$07
-	STA $0342,X
-	LDA #$00
-	STA $0348,X
-	STA $0349,X
-	STA RY
-	RTS
-;************************************************
-;RUTINA QUE LEE LO TECLEADO
-;************************************************
-RUTLEE
-	LDX # <FLSH
-	LDY # >FLSH
-	LDA #$10
-	STX $0228
-	STY $0229
-	STA $021A
-	JSR OPENK
-GETEC
-	JSR $E456
-	CMP #$7E
-	BNE C0
-	LDY RY
-	BEQ GETEC
-	LDA #$00
-	STA (PCRSR),Y
-	LDA #63		;$3F
-	DEY
-	STA (PCRSR),Y
-	DEC RY
-	JMP GETEC
-C0
-	CMP #155	;$9B
-	BEQ C2
-	JSR ASCINT
-	LDY RY
-	STA (PCRSR),Y
-	CPY #20		;#14
-	BEQ C1
-	INC RY
-C1
-	JMP GETEC
-C2
-	JSR CLOSE
-	LDA #$00
-	STA $021A
-	LDY RY
-	STA (PCRSR),Y
-	RTS
-;************************************************
-;DISPLAY DE INICIO DEL PROGRAMA Y FUNCIONALIDAD
-;DIRECTA A TODAS SUS FUNCIONES
-;************************************************
-DOS
-	JMP ($0C)
-@START
-	JSR DOS
-START
-    LDX #<DLS
-    LDY #>DLS
-    STX $230
-    STY $231
-    LDA #$60
-    STA 710
-    STA 712
-    JSR LIMPIO
-//***********************************************
-// Vamos a poner una interrupcion VBI aqui
-//***********************************************
-	ldy #<VBI
-	ldx #>VBI
-	lda #$07	; Diferida
-	jsr SETVBV	;Setea
-
-;************************************************
-;INGRESAMOS EL TITULO 01
-;************************************************
-	LDX # <TITULO01
-	LDY # >TITULO01
-	STX PCRSR
-	STY PCRSR+1
-	JSR RUTLEE
-;************************************************
-;INGRESAMOS EL TITULO 02
-;************************************************
-	LDX # <TITULO02
-	LDY # >TITULO02
-	STX PCRSR
-	STY PCRSR+1
-	JSR RUTLEE
-;************************************************
-;INGRESAMOS EL TITULO 01
-;************************************************
-	LDX # <FUENTE
-	LDY # >FUENTE
-	STX PCRSR
-	STY PCRSR+1
-	JSR RUTLEE
-    JMP *
-
-INICIO
-    JSR KEM
-    JSR MEM
-    LDX # <@START
-	LDY # >@START
-	LDA #$03
-	STX $02
-	STY $03
-	STA $09
-	LDY #$FF
-	STY $08
-	INY   
-	STY $0244
-    JMP START
-LIMPIO
+CALCULOMEMORIA
+    JSR LIMPIOVAL
+    LDA MEMORIA
+    STA VAL
+    LDA MEMORIA+1
+    STA VAL+1
+    LDA MEMORIA+2
+    STA VAL+2
+    JSR BIN2BCD
+    LDY #7
+    LDX #6
+CALCULOMEMORIA1
+    LDA RESATASCII,Y
+    STA MUESTROMEMORIA,X
+    DEY
+    DEX
+    BPL CALCULOMEMORIA1
+    RTS
+CUALPORTB
+    JSR LIMPIOVAL
+    LDA PORTB
+    STA VAL
+    JSR BIN2BCD
+    LDY #7
+    LDX #2
+CUALPORTB1
+    LDA RESATASCII,Y
+    STA MUESTROPORTB,X
+    DEY
+    DEX
+    BPL CUALPORTB1
+    RTS
+CUANTOSBANCOS
+    JSR LIMPIOVAL
+    LDA BANKOS
+    STA VAL
+    JSR BIN2BCD
+    LDY #7
+    LDX #1
+CUANTOSBANCOS1
+    LDA RESATASCII,Y
+    STA MUESTROBANCOS,X
+    DEY
+    DEX
+    BPL CUANTOSBANCOS1
+    RTS
+LIMPIOFUENTE
     LDX #19
     LDA #$00
-LIMPIO2
-    STA TITULO01,X
-    STA TITULO02,X
+LIMPIOFUENTE1
     STA FUENTE,X
     DEX
-    BPL LIMPIO2
-    LDA #63
-    STA TITULO01
-    STA TITULO02
-    STA FUENTE
-    JSR ESPORTB
-    JSR CUALSISTEMA
-    LDX #6
-    LDA #$10
-LIMPIO3
-    STA MUESTROBYTESLEIDOS,X
-    DEX
-    BPL LIMPIO3
-    LDX #3
-LIMPIO4
-    STA MUESTROBLOQUESLEIDOS,X
-    DEX
-    BPL LIMPIO4
-    JSR LIMPIODATA
-    JSR CALCULOMEMORIA
-    JSR CALCULOBANCOS
+    BPL LIMPIOFUENTE1
     RTS
 LIMPIODATA
-    LDX #39
     LDA #$00
+    LDX #39
 LIMPIODATA1
     STA MUESTRODATA,X
     STA MUESTRODATA+40,X
@@ -311,190 +179,218 @@ LIMPIODATA1
     DEX
     BPL LIMPIODATA1
     RTS
-
-ESPORTB
-    JSR LIMPIOVAL
-    LDA PORTB
-    STA VAL
-    JSR BIN2BCD
-    LDY #7
-    LDX #2
-ESPORTB1
-    LDA RESATASCII,Y
-    STA MUESTROPORTB,X
-    DEY
+LIMPIOBYTESCARGADOS
+    LDA #$10
+    LDX #6
+LIMPIOBYTESCARGADOS1
+    STA MUESTROBYTESLEIDOS,X
     DEX
-    BPL ESPORTB1
+    BPL LIMPIOBYTESCARGADOS1
     RTS
-CUALSISTEMA
-    LDA #<NHP
-    LDX #>NHP
-    LDY SISTEMA
-    CPY #0
-    BEQ FINSISTEMA
-    LDA #<NHP8
-    LDX #>NHP8
-    CPY #1
-    BEQ FINSISTEMA
-    LDA #<NHP9
-    LDX #>NHP9
-    CPY #2
-    BEQ FINSISTEMA
-    LDA #<NHPSTAC
-    LDX #>NHPSTAC
-    CPY #3
-    BEQ FINSISTEMA
-    LDA #<NHPULTRA
-    LDX #>NHPULTRA
-    CPY #4
-    BEQ FINSISTEMA
-    LDA #<NHPSUPER
-    LDX #>NHPSUPER
-FINSISTEMA
-    STA FINSISTEMA2+1
-    STX FINSISTEMA2+2
-    LDX #5
-FINSISTEMA2
-    LDA NHP,X
-    STA MUESTROSISTEMA,X
-    DEX
-    BPL FINSISTEMA2
-    LDA #<BAUDIOS600
-    LDX #>BAUDIOS600
-    CPY #0
-    BEQ FINBAUDIOS
-    LDA #<BAUDIOS800
-    LDX #>BAUDIOS800
-    CPY #1
-    BEQ FINBAUDIOS
-    LDA #<BAUDIOS900
-    LDX #>BAUDIOS900
-    CPY #2
-    BEQ FINBAUDIOS
-    LDA #<BAUDIOSSTAC
-    LDX #>BAUDIOSSTAC
-    CPY #3
-    BEQ FINBAUDIOS
-    LDA #<BAUDIOSULTRA
-    LDX #>BAUDIOSULTRA
-    CPY #4
-    BEQ FINBAUDIOS
-    LDA #<BAUDIOSSUPER
-    LDX #>BAUDIOSSUPER
-FINBAUDIOS
-    STA FINBAUDIOS2+1
-    STX FINBAUDIOS2+2
+LIMPIOBLOQUESCARGADOS
+    LDA #$10
     LDX #3
-FINBAUDIOS2
-    LDA BAUDIOS600,X
-    STA MUESTROBAUD,X
+LIMPIOBLOQUESCARGADOS1
+    STA MUESTROBLOQUESAGRABAR,X
     DEX
-    BPL FINBAUDIOS2
+    BPL LIMPIOBLOQUESCARGADOS1
     RTS
-
-CALCULOMEMORIA
-
-    JSR LIMPIOVAL
-    LDA MEMORIA
-    STA VAL
-    LDA MEMORIA+1
-    STA VAL+1
-    LDA MEMORIA+2
-    STA VAL+2
-    JSR BIN2BCD
-    LDX #7
-CALCULOMEMORIA1
-    LDA RESATASCII,X
-    STA MUESTROMEMORIADISPONIBLE,X
+RESETER
+    JSR CALCULOMEMORIA
+    JSR CUALPORTB
+    JSR CUANTOSBANCOS
+    LDX #19
+    LDA #$00
+RESETER1
+    STA TITULO01,X
+    STA TITULO02,X
     DEX
-    BPL CALCULOMEMORIA1
+    BPL RESETER1
+    JSR LIMPIOFUENTE
+    JSR LIMPIODATA
+    JSR LIMPIOBYTESCARGADOS
+    JSR LIMPIOBLOQUESCARGADOS
+    LDA #63
+    STA TITULO01
+    STA TITULO02
+    STA FUENTE
     RTS
-    
-CALCULOBANCOS
-    JSR LIMPIOVAL
-    LDA BANKOS
-    STA VAL
-    JSR BIN2BCD
-    LDX #2
-    LDY #7
-CALCULOBANCOS1
-    LDA RESATASCII,Y
-    STA MUESTROBANCOSDISPONIBLES,X
+;************************************************
+;DISPLAY DE INICIO DEL PROGRAMA Y FUNCIONALIDAD
+;DIRECTA A TODAS SUS FUNCIONES
+;************************************************
+DOS
+	JMP ($0C)
+@START
+	JSR DOS
+START
+    LDA #<DLS
+    STA $230
+    LDA #>DLS
+    STA $231
+    LDA #$80
+    STA 710
+    STA 712
+    JSR RESETER
+
+//***********************************************
+// Vamos a poner una interrupcion VBI aqui
+//***********************************************
+	LDY #<VBI
+	LDX #>VBI
+	LDA #$07	; Diferida
+	JSR SETVBV	;Setea
+    JMP *
+INICIO
+    JSR KEM			;COPIO LA ROM A LA RAM
+    JSR MEM
+	LDX # <@START
+	LDY # >@START
+	LDA #$03
+	STX $02
+	STY $03
+	STA $09
+	LDY #$FF
+	STY $08
+	INY   
+	STY $0244
+	LDX #0
+	STX SISTEMA		;SETEO SISTEMA
+    JMP START
+
+//Ponemos la rutina VBI aqui al final
+.proc VBI
+FIN_SISTEMA=$07	//Desde 0 a 7
+FIN_BLOQUES=$01 //desde 0 a 2 
+	LDA CONSOL
+	CMP CONSOL_ANTERIOR
+	BEQ FIN
+	STA CONSOL_ANTERIOR
+	CMP #$05
+	BEQ ESSELECT
+    CMP #$06
+    BEQ ESSTART
+	CMP #$03 	//OPTION??
+	BNE FIN		//NO!!
+	LDX SISTEMA
+	CPX #FIN_SISTEMA
+	BNE NO_FIN_SISTEMA
+	LDX #$FF
+NO_FIN_SISTEMA
+	INX
+	STX SISTEMA
+	LDA #<NHP600
+	LDY #>NHP600
+	CPX #$00
+	BEQ SIGUE_VEO
+	LDA #<NHP700
+	LDY #>NHP700
+	CPX #$01
+	BEQ SIGUE_VEO
+	LDA #<NHP800
+	LDY #>NHP800
+	CPX #$02
+	BEQ SIGUE_VEO
+	LDA #<NHP900
+	LDY #>NHP900
+	CPX #$03
+	BEQ SIGUE_VEO
+	LDA #<NHPSTAC
+	LDY #>NHPSTAC
+    CPX #$04
+	BEQ SIGUE_VEO
+	LDA #<NHPULTRA
+	LDY #>NHPULTRA
+    CPX #$05
+	BEQ SIGUE_VEO
+	LDA #<NHPSUPER
+	LDY #>NHPSUPER
+    CPX #$06
+	BEQ SIGUE_VEO
+	LDA #<NHPEXTRA
+	LDY #>NHPEXTRA
+SIGUE_VEO
+	STA LOOP_COPIA+1
+	STY LOOP_COPIA+2
+	LDY #$05
+LOOP_COPIA
+	LDA NHP600,Y
+	STA MUESTROSISTEMA,Y
     DEY
-    DEX
-    BPL CALCULOBANCOS1
-    RTS
-
-.PROC VBI
-    LDA CONSOL
-    CMP CONSOLA_ANTERIOR
-    BEQ FINOPTION
-    STA CONSOLA_ANTERIOR
-    CMP #$03
-    BNE FINOPTION
-
-ESOPTION
-    LDY SISTEMA
-    CPY #6
-    BEQ SISTEMAA0
-    INY
-    STY SISTEMA
-ESOPTION2
-    JSR CUALSISTEMA
-FINOPTION
+	BPL LOOP_COPIA
+    JMP SIGUE_VEO1
+FIN
 	JMP $E462
-SISTEMAA0
-    LDY #0
-    STY SISTEMA
-    JMP ESOPTION2
-CONSOLA_ANTERIOR
-    .BY $00
-.ENDP
-    RTS
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ESSTART
+    JMP START
+CONSOL_ANTERIOR
+	.BY $00
+ESSELECT
+    LDX BYTESPORBLOQUE
+	CPX #FIN_BLOQUES
+	BNE NO_FIN_BLOQUES
+	LDX #$FF
+NO_FIN_BLOQUES
+    INX
+	STX BYTESPORBLOQUE
+    LDA #<BYTE128
+	LDY #>BYTE128
+	CPX #$00
+	BEQ SIGUE_VEO_BLOQUES
+    LDA #<BYTE255
+	LDY #>BYTE255
+SIGUE_VEO_BLOQUES
+    STA LOOP_COPIA_BLOQUES+1
+    STY LOOP_COPIA_BLOQUES+2
+    LDY #2
+LOOP_COPIA_BLOQUES
+    LDA BYTE128,Y
+    STA MUESTROBYTESPORBLOQUE,Y
+    DEY
+    BPL LOOP_COPIA_BLOQUES
+    JMP FIN
+;CAMBIAMOS BYTE POR BLOQUES
+	JMP START
+SIGUE_VEO1
+    LDA #<VEL600
+    LDY #>VEL600
+    CPX #0
+    BEQ SIGUE_VEO2
+    LDA #<VEL700
+    LDY #>VEL700
+    CPX #1
+    BEQ SIGUE_VEO2
+    LDA #<VEL800
+    LDY #>VEL800
+    CPX #2
+    BEQ SIGUE_VEO2
+    LDA #<VEL900
+    LDY #>VEL900
+    CPX #3
+    BEQ SIGUE_VEO2
+    LDA #<VELSTAC
+    LDY #>VELSTAC
+    CPX #4
+    BEQ SIGUE_VEO2
+    LDA #<VELULTRA
+    LDY #>VELULTRA
+    CPX #5
+    BEQ SIGUE_VEO2
+    LDA #<VELSUPER
+    LDY #>VELSUPER
+    CPX #6
+    BEQ SIGUE_VEO2
+    LDA #<VELEXTRA
+    LDY #>VELEXTRA
+SIGUE_VEO2
+    STA LOOP_COPIA2+1
+    STY LOOP_COPIA2+2
+    LDY #3
+LOOP_COPIA2
+    LDA VEL600,Y
+    STA MUESTROVELOCIDAD,Y
+    DEY
+    BPL LOOP_COPIA2
+    JMP FIN
+.endp
     RUN INICIO
